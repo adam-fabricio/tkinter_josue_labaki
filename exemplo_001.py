864377272e4ce26f479cebc04b3af2e04b9e705e
Adam <adam@adam.com>
2022-04-24 23:05:49 -0300

    feat: adicionado exemplo 1

---
+++ exemplo_001.py
@@ -0,0 +1,16 @@
+""" Exemplo 001."""
+
+
+from tkinter import *
+
+class Janela:
+	"""Define a Classe que ira criar a janela"""
+	def __init__(self, instancia_de_tk):
+		"""Função init não faz nada nesse exemplo"""
+		pass
+
+raiz = Tk()
+Janela(raiz)
+raiz.mainloop()
+
+	
